82a80fd1078466dc48c5dd261e5a9055a62569a1
ferrypratama <ferryaguspratama@gmail.com>
2026-01-31 21:10:08 +0700

    chore: update project config for aliases and ESM support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,21 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-
+import { fileURLToPath } from "url";
+import path from "path";
 import icon from "astro-icon";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+      },
+    },
   },
 
   integrations: [icon()],
