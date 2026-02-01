f455fd8d541a7f3e1d816871bdd7eff5f09f78cc
ferrypratama <ferryaguspratama@gmail.com>
2026-02-01 20:57:25 +0700

    refactor: restructure stack component into subfolder and update styles

--- src/lib/data/index.ts
+++ src/lib/data/index.ts
@@ -1,9 +1,6 @@
 // Common
 export * from "./common";
 // Sections
-export * from "./common/code-card";
-export * from "./common/github";
-export * from "./common/logo";
-export * from "./common/navigation";
 export * from "./sections/hero";
 export * from "./sections/approach";
+export * from "./sections/stack";
